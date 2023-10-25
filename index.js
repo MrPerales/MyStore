c1a90898e0a9eb027b28d2f595bf5672f56ec928
Carlos Perales <carlosperales050@gmail.com>
2023-10-25 17:54:24 -0600

    instalacion de Express.js y servidor http

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, resp) => {
+  resp.send('hello this is my first server with express');
+});
+
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`);
+});
+
 console.log('my-app');
 console.log('my-app');
