7d75aa0d5dd06609fb758365a4f72ef53ef3c1f9
Carlos Perales <carlosperales050@gmail.com>
2023-11-14 19:22:05 -0600

    add : viariables de entorno

---
+++ config/config.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const config = {
+  env: process.env.NODE_ENV || 'dev',
+  port: process.env.PORT || 3000,
+  dbUser: process.env.DB_USER,
+  dbPassword: process.env.DB_PASSWORD,
+  dbHost: process.env.DB_HOST,
+  dbName: process.env.DB_NAME,
+  dbPort: process.env.DB_PORT,
+  pgMail: process.env.PG_MAIL,
+  pgPassword: process.env.PG_PASSWORD,
+  pgPort: process.env.PG_PORT,
+  pgRefPort: process.env.PG_REFPORT,
+};
+module.exports = { config };
--- libs/postgres.pool.js
+++ libs/postgres.pool.js
@@ -1,12 +1,11 @@
 const { Pool } = require('pg');
+const { config } = require('../config/config');
+
+const USER = encodeURIComponent(config.dbUser);
+const PASSWORD = encodeURIComponent(config.dbPassword);
+const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
 
 // pool
-const pool = new Pool({
-  host: 'localhost',
-  port: 5432,
-  user: 'carlos',
-  password: 'admin123',
-  database: 'my_first_store',
-});
+const pool = new Pool({ connectionString: URI });
 
 module.exports = pool;
