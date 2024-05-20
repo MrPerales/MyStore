6f4be5e2ae3cd56975e32dbb58e9c6260587f14a
Carlos Perales <carlosperales050@gmail.com>
2024-05-20 16:40:42 -0600

    add: categories (services, routes,schemas)

--- Routes/categories.js
+++ Routes/categories.js
@@ -2,28 +2,100 @@ const express = require('express');
 const router = express.Router();
 const CategoryService = require('../services/categoryService');
 const validatorHandler = require('../middlewares/validatorHandler');
+const {
+  getCategorySchema,
+  updateCategorySchema,
+  createCategorySchema,
+  deleteCategorySchema,
+} = require('../schemas/categorySchema');
 
 const service = new CategoryService();
 
-router.get('/', (req, resp) => {
-  resp.json([
-    {
-      categoryId: 1,
-      name: 'games',
-      products: `/products/1`,
-    },
-    {
-      categoryId: 2,
-      name: 'clothes',
-      products: `/products/2`,
-    },
-    {
-      categoryId: 3,
-      name: 'electronics',
-      products: `/products/3`,
-    },
-  ]);
+// router.get('/', (req, resp) => {
+//   resp.json([
+//     {
+//       categoryId: 1,
+//       name: 'games',
+//       products: `/products/1`,
+//     },
+//     {
+//       categoryId: 2,
+//       name: 'clothes',
+//       products: `/products/2`,
+//     },
+//     {
+//       categoryId: 3,
+//       name: 'electronics',
+//       products: `/products/3`,
+//     },
+//   ]);
+// });
+
+router.get('/', async (req, resp, next) => {
+  try {
+    const categories = await service.CategoryFindWithDB();
+    resp.status(200).json(categories);
+  } catch (error) {
+    next(error);
+  }
 });
+
+router.get(
+  '/:id',
+  validatorHandler(getCategorySchema, 'params'),
+  async (req, resp, next) => {
+    try {
+      const { id } = req.params;
+      const category = await service.findOne(id);
+      resp.status(200).json(category);
+    } catch (error) {
+      next(error);
+    }
+  },
+);
+
+router.post(
+  '/',
+  validatorHandler(createCategorySchema, 'body'),
+  async (req, resp, next) => {
+    try {
+      const body = req.body;
+      const newCategory = await service.update(body);
+      resp.status(200).json(newCategory);
+    } catch (error) {
+      next(error);
+    }
+  },
+);
+router.patch(
+  '/:id',
+  validatorHandler(getCategorySchema, 'params'),
+  validatorHandler(updateCategorySchema, 'body'),
+  async (req, resp, next) => {
+    try {
+      const { id } = req.params;
+      const body = req.body;
+      const updateCategory = await service.update(id, body);
+      resp.status(200).json(updateCategory);
+    } catch (error) {
+      next(errro);
+    }
+  },
+);
+router.delete(
+  '/id',
+  validatorHandler(deleteCategorySchema, 'params'),
+  async (req, resp, next) => {
+    try {
+      const { id } = req.params;
+      await service.delete(id);
+      resp.status(201).json({ id });
+    } catch (error) {
+      next(error);
+    }
+  },
+);
+
 // endpoint categories
 router.get('/:categoryId/product/:productId', (req, resp) => {
   const { categoryId, productId } = req.params;
--- services/categoryService.js
+++ services/categoryService.js
@@ -1,4 +1,4 @@
-const bom = require('@hapi/boom');
+const boom = require('@hapi/boom');
 const sequelize = require('../libs/sequelize');
 
 class CategoryService {
@@ -8,6 +8,23 @@ class CategoryService {
     // tiene que ser el mismo ombre que tiene en modelName en la classs Category en este caso
     const response = await sequelize.models.Category.findAll();
     return response;
+  }
+  async findOne(id) {
+    const category = await sequelize.models.Category.findByPk(id);
+    if (!category) {
+      throw boom.notFound('Category not found');
+    }
+    return category;
+  }
+  async update(id, changes) {
+    const category = await this.findOne(id);
+    const resp = await category.update(changes);
+    return resp;
+  }
+  async delete(id) {
+    const category = await this.findOne(id);
+    await category.destroy();
+    return { id };
   }
 }
 
