34daab97dd19b3d3cea2c37cff68bd7b9d4dd7de
Carlos Perales <carlosperales050@gmail.com>
2024-05-31 18:23:41 -0600

    config localstrategy

--- utils/auth/strategies/localStrategy.js
+++ utils/auth/strategies/localStrategy.js
@@ -4,25 +4,32 @@ const bcrypt = require('bcrypt');
 const UsersService = require('./../../../services/userServices');
 const service = new UsersService();
 
-const localStratgy = new Strategy(async (email, password, done) => {
-  try {
-    const user = await service.findByEmail(email);
-    if (!user) {
+const localStratgy = new Strategy(
+  {
+    usernameField: 'email',
+    passwordField: 'password',
+  },
+  async (email, password, done) => {
+    try {
+      const user = await service.findByEmail(email);
+      if (!user) {
+        // manda el error , no fue posible la conexion
+        done(boom.unauthorized(), false);
+      }
+      const hash = user.password;
+      const isMatch = await bcrypt.compare(password, hash);
+      if (!isMatch) {
+        // manda el error , no fue posible la conexion
+        done(boom.unauthorized(), false);
+      }
+      delete user.dataValues.password;
+      // null = no hay error , mandamos info
+      done(null, user);
+    } catch (error) {
       // manda el error , no fue posible la conexion
-      done(boom.unauthorized(), false);
+      done(error, false);
     }
-    const hash = user.password;
-    const isMatch = await bcrypt.compare(password, hash);
-    if (!isMatch) {
-      // manda el error , no fue posible la conexion
-      done(boom.unauthorized(), false);
-    }
-    // null = no hay error , mandamos info
-    done(null, user);
-  } catch (error) {
-    // manda el error , no fue posible la conexion
-    done(error, false);
-  }
-});
+  },
+);
 
 module.exports = localStratgy;
